7c07316f3c21f30220963e413acc732b77d1a34f
Reese M Bowman <reesemb1@gmail.com>
2020-01-03 22:20:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,8 @@
 def reverse_each_word(str)
   array = str.split(" ")
-<<<<<<< HEAD
   flip = []
   array.collect do |word|
     flip << word.reverse
   end
   flip.join(" ")
-=======
-  str = []
-  array.collect do |word|
-    str << word.reverse
-    str.join()
-  end
-  str
->>>>>>> 5b5b79a9dc07d9b3c11095747204f0ccbce4cbf4
 end
